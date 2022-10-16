93431aebca781d9e76c7871f51b54dbf898be410
Cecília <ceciliagarcia@usp.br>
2022-10-16 20:53:06 -0300

    Preparando aplicação para deploy

--- src/db/psql.js
+++ src/db/psql.js
@@ -4,19 +4,19 @@ dotenv.config();
 
 const { Pool } = pg;
 
-const connection = new Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-});
-// const databaseConfig = {
-//     connectionString: process.env.DATABASE_URL,
-//     ssl: {
-//         rejectUnauthorized: true
-//     }
-// }
-// const connection = new Pool(databaseConfig);
+// const connection = new Pool({
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+// });
+const databaseConfig = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: true
+    }
+}
+const connection = new Pool(databaseConfig);
 
 export default connection;
